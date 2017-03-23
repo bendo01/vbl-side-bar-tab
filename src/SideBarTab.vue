4a83ca741c1e4d9e01bd1b82688424bf4a412750
Benny L.E.P <bendo01@gmail.com>
2017-03-24 02:26:04 +0800

    first init

---
+++ src/SideBarTab.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="tab-pane" :class="{ 'is-active':isActive }">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+'use strict';
+export default {
+    props: {
+        icon: { required: true },
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data () {
+        return {
+            isActive: false,
+            iconData: this.icon
+        };
+    },
+    mounted () {
+        this.isActive = this.selected;
+    }
+};
+</script>
